3a672d7b85f624e4c511d3ce20c56194158fd4c0
favio <faugconti@gmail.com>
2021-03-07 02:33:01 -0300

    fix css

--- src/components/UI/Star/Star.js
+++ src/components/UI/Star/Star.js
@@ -1,74 +1,71 @@
-import React from 'react';
-import classes from './Star.module.css'
-import {connect} from 'react-redux';
-import * as actions from '../../../store/actions/index';
+import React from "react";
+// import classes from './Star.module.css'
+import { connect } from "react-redux";
+import * as actions from "../../../store/actions/index";
+import "../../../App.css";
 
-const Star = props => {
+const Star = (props) => {
+  // let [classStyle,setClassStyle] = useState(!props.favorite ? 'ADD' : 'REMOVE')
+  // let classStyle = props.favorite ? 'REMOVE' :'ADD';
 
-    // let [classStyle,setClassStyle] = useState(!props.favorite ? 'ADD' : 'REMOVE')
-    // let classStyle = props.favorite ? 'REMOVE' :'ADD';
+  const elementClasses = ['boton'];
 
-    const elementClasses = [classes.Button];
+  if (!props.favorite) {
+    elementClasses.push("ADD");
+  } else {
+    elementClasses.push("REMOVE");
+  }
 
-    if(!props.favorite){
-        elementClasses.push([classes['ADD']]);
-    }else{
-        elementClasses.push([classes['REMOVE']])
+  const clickHandler = async (e) => {
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+    if (!props.loading) {
+      if (props.favorite) {
+        // console.log(elementClasses)
+        const newList = props.movieList.filter((movie) => {
+          return movie.imdbID !== props.imdbID;
+        });
+        // console.log('new list ',newList);
+        await props.updateMovieList(newList);
+
+        elementClasses.pop();
+        elementClasses.push("ADD");
+        // console.log(elementClasses)
+      } else {
+        const newList = props.movieList.concat([props.currentMovie]);
+        await props.updateMovieList(newList);
+        // console.log('new list ',newList);
+        elementClasses.pop();
+        elementClasses.push("REMOVE");
+        // console.log(elementClasses)
+      }
     }
-
-    const clickHandler = (e) =>{
-        e.stopPropagation();
-        e.nativeEvent.stopImmediatePropagation();
-        if(!props.loading){
-            
-            if(props.favorite){
-                // console.log(elementClasses)
-                const newList = props.movieList.filter((movie) => {
-                    return movie.imdbID !== props.imdbID
-                })
-                // console.log('new list ',newList);
-                props.updateMovieList(newList);
-
-                elementClasses.pop();
-                elementClasses.push(classes['ADD'])
-                // console.log(elementClasses)
-            }else{
-                const newList = props.movieList.concat([props.currentMovie])
-                props.updateMovieList(newList);
-                // console.log('new list ',newList);
-                elementClasses.pop();
-                elementClasses.push(classes['REMOVE'])
-                // console.log(elementClasses)
-            }
-        }
-        
-    }
-
-    return(
-        <div>
-            <div 
-                onClick={clickHandler}
-                className={elementClasses.join(' ')}
-            > 
-                {!props.favorite ? '+' : '-'}   
-            </div> 
-        </div>
-          
-    )
-
+  };
+  console.log(elementClasses.join(" "));
+  return (
+    <div>
+      <div
+        // className="boton"
+        onClick={clickHandler}
+        className={elementClasses.join(' ')}
+      >
+        {!props.favorite ? "+" : "-"}
+      </div>
+    </div>
+  );
 };
 
-const mapStateToProps = state => {
-    return{
-        movieList : state.movies.movieList,
-        loading: state.movies.loading
-    }
+const mapStateToProps = (state) => {
+  return {
+    movieList: state.movies.movieList,
+    loading: state.movies.loading,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return{
-        updateMovieList: (movies) => dispatch(actions.editMovies(movies))
-    }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateMovieList: (movies) => dispatch(actions.editMovies(movies)),
+  };
 };
 
-export default connect(mapStateToProps,mapDispatchToProps)(Star);
+export default connect(mapStateToProps, mapDispatchToProps)(Star);
